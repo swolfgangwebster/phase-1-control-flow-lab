7f9c4da67f07ade0aa7f0b8aeac02d0a45eccb7b
Shawn Webster <swolfgangwebster@gmail.com>
2023-03-14 21:20:33 -0500

    uber

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(d){
   // Write your code here!
+  if (d <= 400) {return 'This one is on me!'}
+  else if (d > 400 && d <= 2000) {return 'That will be twenty bucks.'}
+  else if (d > 2000 && d<=2500) {return 'I will gladly take your thirty bucks.'}
+  else {return 'No can do.'}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(c){
   // Write your code here!
+  return c == "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(t){
   // Write your code here!
+  switch (t) {
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous':
+      return "Thank you."
+    default:
+        return 'Bye.'
+  }
 }
